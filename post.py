911303a6fa25fe9b5c27947fcd68d25d6db08a24
Denil Gevariya <gevariyadenil0055@gmail.com>
2025-11-10 17:37:58 +0530

    Update channel username in post.py

--- post.py
+++ post.py
@@ -4,7 +4,7 @@ import json
 
 # ================= CONFIG =================
 BOT_TOKEN = "8284958888:AAFrNeQ7FRY9tNzyMV5B3npn9DneCDsBOhs"   # <-- PUT YOUR BOT TOKEN
-CHANNEL = "@producttestupdates"  # <-- PUT YOUR CHANNEL USERNAME
+CHANNEL = "@HirparaOnlineHub"  # <-- PUT YOUR CHANNEL USERNAME
 SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/12Ed5V6MtxAX4YYew7Ba3mAcZyM83DpVyjin8OIcnWEU/export?format=csv&gid=0"
 # ==========================================
 
